f055680436ec18c202dca9836ed86a540730a8ac
RayhanHamada <hamadadev2016@gmail.com>
2021-02-01 00:38:08 +0700

    add discord.js-commando

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { CommandoClient } from 'discord.js-commando';
+import config from './command-config.json';
+
+const client = new CommandoClient({
+  commandPrefix: config.prefix,
+});
+
+client.registry.registerDefaultCommands();
+
